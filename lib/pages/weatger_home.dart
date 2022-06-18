a30aad915d89e0175fa6afccb5fa58835a5d10eb
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-06-19 01:08:29 +0600

    current weather displayed

--- lib/pages/weatger_home.dart
+++ lib/pages/weatger_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/providers/weather_provider.dart';
 
 class WeatherHome extends StatefulWidget {
   const WeatherHome({Key? key}) : super(key: key);
@@ -8,24 +10,42 @@ class WeatherHome extends StatefulWidget {
 }
 
 class _WeatherHomeState extends State<WeatherHome> {
+  late WeatherProvider weatherProvider;
+  bool isLoading=true;
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    weatherProvider = Provider.of<WeatherProvider>(context, listen: false);
+    double  date =weatherProvider.getCurrentWeatherData.main.temp;
+    weatherProvider.fetchCurrentData().then((_)  {
+      setState(() {
+        isLoading=false;
+      });
+    });
+    super.didChangeDependencies();
+  }
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
       appBar: AppBar(
         title: const Text('Weather Home'),
       ),
-      body:  Center(
-        child: Column(
-          children: [
-            const Text( 'June 18 2022',style: TextStyle(fontSize: 30,)),
-            const Text( '28\u00B0C',style: TextStyle(fontSize: 65,fontWeight: FontWeight.bold),),
-            const Text( 'feels like: 40\u00B0C',style: TextStyle(fontSize: 30,),),
-            Row(
-              children:  [
-                Image.network('https://sa.kapamilya.com/absnews/abscbnnews/media/2022/news/05/18/jma.jpg',width: 80,height: 80,fit: BoxFit.cover,),
-                const Text("Heavy Rain", style: TextStyle(fontSize: 30),)
-              ],)
-          ],
+      body: isLoading? const Center(child: CircularProgressIndicator(),)
+          :
+      Center(
+        child: Consumer<WeatherProvider>(
+          builder: (context, provider, _)=> Column(
+            children: [
+              const Text( 'June 18 2022',style: TextStyle(fontSize: 30,)),
+              Text( '${provider.getCurrentWeatherData.main.temp.round()}\u00B0C',style: const TextStyle(fontSize: 60,),),
+              Text( 'feels like: ${provider.getCurrentWeatherData.main.feelsLike.round()}\u00B0C',style: const TextStyle(fontSize: 30,),),
+              Row(
+                children:  [
+                  Image.network('https://sa.kapamilya.com/absnews/abscbnnews/media/2022/news/05/18/jma.jpg',width: 80,height: 80,fit: BoxFit.cover,),
+                  const Text("Heavy Rain", style: TextStyle(fontSize: 30),)
+                ],)
+            ],
+          ),
         ),
       ),
     );
--- lib/providers/weather_provider.dart
+++ lib/providers/weather_provider.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/models/current_model.dart';
 import 'package:weather_app/models/forecast_weather.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_app/weather_utils.dart';
 
 class WeatherProvider extends ChangeNotifier{
   late CurrentWeatherRes _currert;
@@ -12,14 +13,14 @@ class WeatherProvider extends ChangeNotifier{
   ForecastWeatherRes get getCurrentForecastData=>_forecast;
 
   Future fetchCurrentData() async{
-    final url ='https://api.openweathermap.org/data/2.5/weather?q=dhaka&units=metric&appid=e7e6bbb42b41e6995fd516a684044c88';
+    final url ='https://api.openweathermap.org/data/2.5/weather?q=dhaka&units=metric&appid=$weather_api_key';
     final res = await http.get(Uri.parse(url));
     final resMap = json.decode(res.body);
     _currert=CurrentWeatherRes.fromJson(resMap);
     notifyListeners();
   }
   Future fetchForecastData() async{
-    final url ='https://api.openweathermap.org/data/2.5/forecast?q=dhaka&units=metric&appid=e7e6bbb42b41e6995fd516a684044c88';
+    final url ='https://api.openweathermap.org/data/2.5/forecast?q=dhaka&units=metric&appid=$weather_api_key';
     final res = await http.get(Uri.parse(url));
     final resMap = json.decode(res.body);
     _forecast=ForecastWeatherRes.fromJson(resMap);
