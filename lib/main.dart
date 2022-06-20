9ac8394f6a15e9345361554e80ca31aa287f3932
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-06-20 13:05:07 +0600

    current weather backend 100%

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:weather_app/pages/weatger_home.dart';
+import 'package:weather_app/pages/weather_home.dart';
 import 'package:weather_app/providers/weather_provider.dart';
 
 void main() {
