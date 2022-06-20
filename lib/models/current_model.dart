8a712277777e35f773f85b99212985dfc0fda833
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-06-20 11:17:51 +0600

    diffrent city bug fix

--- lib/models/current_model.dart
+++ lib/models/current_model.dart
@@ -146,30 +146,23 @@ class Main {
 
 class Sys {
   Sys({
-   required this.type,
-   required this.id,
    required this.country,
    required this.sunrise,
    required this.sunset,
   });
 
-  int type;
-  int id;
+
   String country;
   int sunrise;
   int sunset;
 
   factory Sys.fromJson(Map<String, dynamic> json) => Sys(
-    type: json["type"],
-    id: json["id"],
     country: json["country"],
     sunrise: json["sunrise"],
     sunset: json["sunset"],
   );
 
   Map<String, dynamic> toJson() => {
-    "type": type,
-    "id": id,
     "country": country,
     "sunrise": sunrise,
     "sunset": sunset,
